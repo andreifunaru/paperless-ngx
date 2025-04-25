093ab220ef7cf49251891ede73b391bedc10a746
shamoon <4887959+shamoon@users.noreply.github.com>
2025-04-24 22:21:41 -0700

    Add backend settings to frontend config
    [ci skip]

--- src-ui/src/app/data/paperless-config.ts
+++ src-ui/src/app/data/paperless-config.ts
@@ -53,6 +53,11 @@ export const ConfigCategory = {
   AI: $localize`AI Settings`,
 }
 
+export const LLMEmbeddingBackendConfig = {
+  OPENAI: 'openai',
+  LOCAL: 'local',
+}
+
 export const LLMBackendConfig = {
   OPENAI: 'openai',
   OLLAMA: 'ollama',
@@ -195,6 +200,21 @@ export const PaperlessConfigOptions: ConfigOption[] = [
     category: ConfigCategory.AI,
   },
   {
+    key: 'llm_embedding_backend',
+    title: $localize`LLM Embedding Backend`,
+    type: ConfigOptionType.Select,
+    choices: mapToItems(LLMEmbeddingBackendConfig),
+    config_key: 'PAPERLESS_LLM_EMBEDDING_BACKEND',
+    category: ConfigCategory.AI,
+  },
+  {
+    key: 'llm_embedding_model',
+    title: $localize`LLM Embedding Model`,
+    type: ConfigOptionType.String,
+    config_key: 'PAPERLESS_LLM_EMBEDDING_MODEL',
+    category: ConfigCategory.AI,
+  },
+  {
     key: 'llm_backend',
     title: $localize`LLM Backend`,
     type: ConfigOptionType.Select,
@@ -242,6 +262,8 @@ export interface PaperlessConfig extends ObjectWithId {
   app_logo: string
   app_title: string
   ai_enabled: boolean
+  llm_embedding_backend: string
+  llm_embedding_model: string
   llm_backend: string
   llm_model: string
   llm_api_key: string
